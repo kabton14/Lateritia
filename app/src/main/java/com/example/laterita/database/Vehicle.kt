04b5dad6cd8ffcea574a02dfb3a89adf16ee6a96
Kevin Beaton <kabton14@gmail.com>
2021-12-26 23:07:20 -0500

    Add an entity

---
+++ app/src/main/java/com/example/laterita/database/Vehicle.kt
@@ -0,0 +1,29 @@
+package com.example.laterita.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "vehicles")
+class Vehicle (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "make")
+    val make: String,
+
+    @ColumnInfo(name = "model")
+    val model: String,
+
+    @ColumnInfo(name = "vin")
+    val vin: String,
+
+    @ColumnInfo(name = "licence")
+    val licence: String,
+
+    @ColumnInfo(name = "fuel_capacity")
+    val fuelCapacity: Int,
+
+    @ColumnInfo(name = "reserve_capacity")
+    val reserveCapacity: Int = 6
+)
