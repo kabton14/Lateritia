25bbf0f32e11684c275b930d40d149f2ee79dbc4
Kevin Beaton <kabton14@gmail.com>
2021-12-27 23:07:42 -0500

    Create the HomeViewModel

---
+++ app/src/main/java/com/example/laterita/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.laterita.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.laterita.database.VehicleDao
+
+class HomeViewModel(private val vehicleDao: VehicleDao): ViewModel() {
+}
+
