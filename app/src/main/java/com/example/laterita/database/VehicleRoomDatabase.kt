c509cd4c933bf69f7e872cdb02aa7f41b37ecf1c
Kevin Beaton <kabton14@gmail.com>
2021-12-28 13:41:41 -0500

    Add volatile annotation to instance variable and remove incorrect imports

--- app/src/main/java/com/example/laterita/database/VehicleRoomDatabase.kt
+++ app/src/main/java/com/example/laterita/database/VehicleRoomDatabase.kt
@@ -4,17 +4,16 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [Vehicle::class], version = 1, exportSchema = false)
 abstract class VehicleRoomDatabase : RoomDatabase() {
     abstract fun vehicleDao(): VehicleDao
 
     companion object {
+
+        @Volatile
         private var INSTANCE: VehicleRoomDatabase? = null
 
-        @InternalCoroutinesApi
         fun getDatabase(context: Context): VehicleRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
