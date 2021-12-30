6bd5ecea2db6059d2612ba037983d85a6d6ea73b
Kevin Beaton <kabton14@gmail.com>
2021-12-30 13:53:42 -0500

    Create Live data variable for vehicle and encapsulate it

--- app/src/main/java/com/example/laterita/home/HomeViewModel.kt
+++ app/src/main/java/com/example/laterita/home/HomeViewModel.kt
@@ -1,18 +1,28 @@
 package com.example.laterita.home
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import com.example.laterita.database.Vehicle
 import com.example.laterita.database.VehicleDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeViewModel(private val vehicleDao: VehicleDao): ViewModel() {
+
+    private var _vehicle = MutableLiveData<Vehicle?>()
+    val vehicle: LiveData<Vehicle?>
+        get() = _vehicle
+
 }
 
-class HomeViewModelFactory(private val vehicleDao: VehicleDao): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return HomeViewModel(vehicleDao) as T
+    class HomeViewModelFactory(private val vehicleDao: VehicleDao) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return HomeViewModel(vehicleDao) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
-}
+
+
