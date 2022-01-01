caf488e054a20f9f9b4481a4c8859f8177b3a467
Kevin Beaton <kabton14@gmail.com>
2021-12-31 22:37:22 -0500

    Connect to view model

--- app/src/main/java/com/example/laterita/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/laterita/settings/SettingsFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.laterita.R
+import com.example.laterita.database.VehicleRoomDatabase
 import com.example.laterita.databinding.FragmentSettingsBinding
 import com.example.laterita.home.HomeFragment
 
@@ -27,11 +29,27 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        // Get a reference to the binding object and inflate the fragment views.
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings,
             container, false)
 
         val application = requireNotNull(this.activity).application
         val arguments = SettingsFragmentArgs.fromBundle(requireArguments())
+
+        // Create an instance of the ViewModel Factory.
+        val dataSource = VehicleRoomDatabase.getDatabase(application).vehicleDao()
+        val viewModelFactory = SettingsViewModelFactory(arguments.vehicleKey, dataSource)
+
+        // Get a reference to the ViewModel associated with this fragment.
+        val settingsViewModelViewModel =
+            ViewModelProvider(
+                this, viewModelFactory).get(SettingsViewModel::class.java)
+
+        // To use the View Model with data binding, you have to explicitly
+        // give the binding object a reference to it.
+        binding.settingsViewModel = settingsViewModelViewModel
+
+        binding.lifecycleOwner = this
 
         return binding.root
 
@@ -40,9 +58,9 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.buttonSecond.setOnClickListener {
-//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-//        }
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_settings_fragment_to_home_fragment)
+        }
     }
 
     override fun onDestroyView() {
