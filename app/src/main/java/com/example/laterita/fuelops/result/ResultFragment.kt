a97cd1923c5d0637d2f3dc2d43e779060470f524
Kevin Beaton <kabton14@gmail.com>
2022-01-08 17:18:24 -0500

    Add Result fragment

---
+++ app/src/main/java/com/example/laterita/fuelops/result/ResultFragment.kt
@@ -0,0 +1,50 @@
+package com.example.laterita.fuelops.result
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.laterita.R
+import com.example.laterita.databinding.FragmentResultBinding
+import com.example.laterita.fuelops.OperationsViewModel
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ResultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val fuelOpsViewModel: OperationsViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container,
+            false)
+        binding.operationsViewModel = fuelOpsViewModel
+        binding.lifecycleOwner = this
+
+        //Live data observers
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
