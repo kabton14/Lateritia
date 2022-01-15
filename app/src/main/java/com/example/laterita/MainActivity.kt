845f7ea1bcb4e1298deaefab190fae7e3d12e08c
Kevin Beaton <kabton14@gmail.com>
2022-01-15 05:22:52 -0500

    Add Add navigation to action bar

--- app/src/main/java/com/example/laterita/MainActivity.kt
+++ app/src/main/java/com/example/laterita/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.laterita
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var appBarConfiguration : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.lateritia_toolbar))
 
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+
     }
-    
 }
