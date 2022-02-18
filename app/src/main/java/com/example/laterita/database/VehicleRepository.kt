e88f49cd45e6feed124ee3cfb755802c6105fb7d
Kevin Beaton <kabton14@gmail.com>
2022-02-18 01:36:46 -0500

    Add default argument

--- app/src/main/java/com/example/laterita/database/VehicleRepository.kt
+++ app/src/main/java/com/example/laterita/database/VehicleRepository.kt
@@ -1,7 +1,7 @@
 package com.example.laterita.database
 
 class VehicleRepository(private val vehicleDao: VehicleDao) {
-    suspend fun getVehicle(id: Long): Vehicle? {
+    suspend fun getVehicle(id: Long = 14): Vehicle? {
         return vehicleDao.loadVehicle(id)
     }
 }
