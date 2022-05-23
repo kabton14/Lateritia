fef5211985e4675f8387097b72744d332323fd9a
Kevin Beaton <kabton14@gmail.com>
2022-05-23 05:10:05 -0500

    Set appbar icon

--- app/src/main/java/com/example/lateritia/MainActivity.kt
+++ app/src/main/java/com/example/lateritia/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.lateritia_toolbar))
+        supportActionBar?.setIcon(R.drawable.ic_logo_main)
 
         val navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
