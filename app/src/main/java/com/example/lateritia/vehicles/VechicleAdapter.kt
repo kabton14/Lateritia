6535dd5ebfa0271a9c3b142a46b086b72321be8d
Kevin Beaton <kabton14@gmail.com>
2022-06-15 15:30:14 -0500

    Get references to the views

--- app/src/main/java/com/example/lateritia/vehicles/VechicleAdapter.kt
+++ app/src/main/java/com/example/lateritia/vehicles/VechicleAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.lateritia.vehicles
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.lateritia.R
 import com.example.lateritia.database.Vehicle
 
 class VehicleAdapter: RecyclerView.Adapter<VehicleAdapter.ViewHolder>(){
     var data = listOf<Vehicle>()
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val vehicleName: TextView = itemView.findViewById(R.id.vehicle_item_title)
+        val licence: TextView = itemView.findViewById(R.id.vehicle_item_info_1)
+        val capacity: TextView = itemView.findViewById(R.id.vehicle_item_info_2)
     }
 }
