1827b6addae704fa1c6d97a758ea6e6fff4cd527
Kevin Beaton <kabton14@gmail.com>
2022-06-15 16:49:59 -0500

    Assign the value to the adapter's data when the value is not null

--- app/src/main/java/com/example/lateritia/vehicles/VehicleListFragment.kt
+++ app/src/main/java/com/example/lateritia/vehicles/VehicleListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.lateritia.R
@@ -38,6 +39,12 @@ class VehicleListFragment : Fragment() {
         binding.lifecycleOwner = this
 
         //Live data observers
+        vehicleListViewModel.vehicles.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                adapter.data = it
+            }
+
+        })
 
         setHasOptionsMenu(true)
         return binding.root
