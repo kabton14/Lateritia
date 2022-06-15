3ba604d864b8adb58e056b7543ac74735cd72ca3
Kevin Beaton <kabton14@gmail.com>
2022-06-15 15:33:31 -0500

    Add stubs

--- app/src/main/java/com/example/lateritia/vehicles/VechicleAdapter.kt
+++ app/src/main/java/com/example/lateritia/vehicles/VechicleAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.lateritia.vehicles
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lateritia.R
@@ -8,6 +9,18 @@ import com.example.lateritia.database.Vehicle
 
 class VehicleAdapter: RecyclerView.Adapter<VehicleAdapter.ViewHolder>(){
     var data = listOf<Vehicle>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val vehicleName: TextView = itemView.findViewById(R.id.vehicle_item_title)
