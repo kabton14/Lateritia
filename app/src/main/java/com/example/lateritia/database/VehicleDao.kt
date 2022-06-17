22431775d7e46a2218c85ec6cb8029c6ac423838
Kevin Beaton <kabton14@gmail.com>
2022-06-17 18:34:07 -0500

    Remove suspend

--- app/src/main/java/com/example/lateritia/database/VehicleDao.kt
+++ app/src/main/java/com/example/lateritia/database/VehicleDao.kt
@@ -7,18 +7,18 @@ import androidx.room.*
 interface VehicleDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(vehicle: Vehicle)
+    fun insert(vehicle: Vehicle)
 
     @Update
-    suspend fun update(vehicle: Vehicle)
+    fun update(vehicle: Vehicle)
 
     @Delete
-    suspend fun delete(vararg vehicles: Vehicle)
+    fun delete(vararg vehicles: Vehicle)
 
     @Query("select * from vehicles where id = :id")
-    suspend fun loadVehicle(id: Long): Vehicle?
+    fun loadVehicle(id: Long): Vehicle?
 
     @Query("select * from vehicles ORDER BY id ASC")
-    fun loadAllVehicles(): LiveData<List<Vehicle>?>
+    fun loadAllVehicles(): LiveData<List<Vehicle>>
 
 }
