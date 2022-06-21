7df71cefb1906dc1599c26492a0aac6f36952010
Kevin Beaton <kabton14@gmail.com>
2022-06-21 02:48:36 -0500

    Refactor

--- app/src/main/java/com/example/lateritia/vehicles/VehicleAdapter.kt
+++ app/src/main/java/com/example/lateritia/vehicles/VehicleAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.lateritia.vehicles
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,46 +9,51 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lateritia.R
 import com.example.lateritia.database.Vehicle
+import com.example.lateritia.databinding.ListItemVehicleBinding
 
-class VehicleAdapter(val current: Int, val clickListener: VehicleListener ):
-    ListAdapter<Vehicle, VehicleAdapter.ViewHolder>(VehicleDiffCallback()){
+class VehicleAdapter(val current: Int ):
+    ListAdapter<Vehicle, VehicleAdapter.ViewHolder>(VehicleDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ListItemVehicleBinding.inflate(layoutInflater, parent, false)
-
-        return ViewHolder(binding)
+        return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
-        bind(holder, item)
+        holder.bind(item)
     }
 
-    private fun bind(
-        holder: ViewHolder,
-        item: Vehicle
-    ) {
-        val res = holder.itemView.context.resources
 
-        holder.vehicleName.text = "${item.make} ${item.model}"
-        holder.vehicleName.isSelected = true
-        holder.licence.text = "${item.licence}"
-        holder.capacity.text = "${item.fuelCapacity}L"
-
-        if (item.id == current.toLong()) {
-            holder.default.setImageResource(R.drawable.ic_default_tick)
-        } else {
-            holder.default.setImageResource(R.drawable.ic_hollow_circle)
-        }
-    }
-
-    class ViewHolder(val binding: ListItemVehicleBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder private constructor(val binding: ListItemVehicleBinding) : RecyclerView.ViewHolder(binding.root) {
         val thumbnail: ImageView = binding.vehicleItemImage
         val vehicleName: TextView = binding.vehicleItemTitle
         val licence: TextView = binding.vehicleItemInfo1
         val capacity: TextView = binding.vehicleItemInfo2
         val default: ImageView = binding.defaultVehicleIndicator
+
+        fun bind(item: Vehicle) {
+            vehicleName.text = "${item.make} ${item.model}"
+            vehicleName.isSelected = true
+            licence.text = "${item.licence}"
+            capacity.text = "${item.fuelCapacity}L"
+
+//            if (item.id == current.toLong()) {
+//                holder.default.setImageResource(R.drawable.ic_default_tick)
+//            } else {
+//                holder.default.setImageResource(R.drawable.ic_hollow_circle)
+//            }
+            binding.vehicle = item
+            binding.executePendingBindings()
+        }
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemVehicleBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+    }
 }
 
 class VehicleDiffCallback : DiffUtil.ItemCallback<Vehicle>() {
@@ -64,6 +68,6 @@ class VehicleDiffCallback : DiffUtil.ItemCallback<Vehicle>() {
 }
 
 class VehicleListener(val onClickListener: (vehicleId: Long) -> Unit) {
-    fun onClick(vehicle: Vehicle) = onClickListener(vehicle.id)
+    fun onClick(vehicle: Long) = onClickListener(vehicle)
 
 }
