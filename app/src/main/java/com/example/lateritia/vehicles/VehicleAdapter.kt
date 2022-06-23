bafaf29ae5febfc3f8076ac2d307ae4808d89b65
Kevin Beaton <kabton14@gmail.com>
2022-06-23 15:26:12 -0500

    Remove vehicle id param

--- app/src/main/java/com/example/lateritia/vehicles/VehicleAdapter.kt
+++ app/src/main/java/com/example/lateritia/vehicles/VehicleAdapter.kt
@@ -8,8 +8,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lateritia.database.Vehicle
 import com.example.lateritia.databinding.ListItemVehicleBinding
 
-class VehicleAdapter(val current: Long, private val clickListener: VehicleListener,
-                     private val vmb:VehicleListViewModel ):
+class VehicleAdapter(private val vehicleListViewModel:VehicleListViewModel, 
+                     private val clickListener: VehicleListener):
     ListAdapter<Vehicle, VehicleAdapter.ViewHolder>(VehicleDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,17 +18,16 @@ class VehicleAdapter(val current: Long, private val clickListener: VehicleListen
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
-        holder.bind(item, current, clickListener, vmb)
+        holder.bind(item, vehicleListViewModel, clickListener)
     }
 
 
     class ViewHolder private constructor(val binding: ListItemVehicleBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: Vehicle, current: Long, clickListener: VehicleListener,
-                 vmb: VehicleListViewModel) {
+        fun bind(item: Vehicle, vehicleListViewModel: VehicleListViewModel, clickListener: VehicleListener) {
             binding.vehicle = item
             binding.vehicleItemTitle.isSelected = true
-            binding.vehicleListViewModel = vmb
+            binding.vehicleListViewModel = vehicleListViewModel
             binding.clickListener = clickListener
             binding.executePendingBindings()
         }
