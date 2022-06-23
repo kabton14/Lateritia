cd518bfbffc8f9a61ae3e4fdc890615fdec73b48
Kevin Beaton <kabton14@gmail.com>
2022-06-23 15:27:56 -0500

    Add default (current) vehicle property

--- app/src/main/java/com/example/lateritia/vehicles/VehicleListViewModel.kt
+++ app/src/main/java/com/example/lateritia/vehicles/VehicleListViewModel.kt
@@ -5,15 +5,20 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.lateritia.database.VehicleDao
 import com.example.lateritia.database.VehicleRepository
 
-class VehicleListViewModel(private val vehicleDao: VehicleDao): ViewModel() {
+class VehicleListViewModel(private val vehicleDao: VehicleDao, private val defaultVehicle: Long)
+    : ViewModel() {
+
+    var currentVehicle: Long = defaultVehicle
+
     val vehicles = vehicleDao.loadAllVehicles()
 }
 
-class VehicleListViewModelFactory(private val vehicleDao: VehicleDao) : ViewModelProvider.Factory {
+class VehicleListViewModelFactory(private val vehicleDao: VehicleDao, private val defaultVehicle: Long)
+    : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(VehicleListViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return VehicleListViewModel(vehicleDao) as T
+            return VehicleListViewModel(vehicleDao, defaultVehicle) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
