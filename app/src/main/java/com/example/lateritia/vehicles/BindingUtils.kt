0dee7f74406333a84f6187031483e2d266be812c
Kevin Beaton <kabton14@gmail.com>
2022-06-27 13:45:53 -0500

    Add binding adapter for fuel gauge level

--- app/src/main/java/com/example/lateritia/vehicles/BindingUtils.kt
+++ app/src/main/java/com/example/lateritia/vehicles/BindingUtils.kt
@@ -22,3 +22,39 @@ fun ImageView.setDefaultIndicatorIcon(vehicle: Vehicle, defaultVehicle: Long) {
         }
     )
 }
+
+@BindingAdapter("vehicle", "level")
+fun ImageView.setFuelGauge(vehicle: Vehicle, level: Int) {
+    setImageResource(
+        when {
+            vehicle?.model?.lowercase()?.contains("axio") == true -> {
+                when (level) {
+                    0 -> R.drawable.ic_fuel_gauge_axio_16_0
+                    1 -> R.drawable.ic_fuel_gauge_axio_16_1
+                    2 -> R.drawable.ic_fuel_gauge_axio_16_2
+                    3 -> R.drawable.ic_fuel_gauge_axio_16_3
+                    4 -> R.drawable.ic_fuel_gauge_axio_16_4
+                    5 -> R.drawable.ic_fuel_gauge_axio_16_5
+                    6 -> R.drawable.ic_fuel_gauge_axio_16_6
+                    7 -> R.drawable.ic_fuel_gauge_axio_16_7
+                    8 -> R.drawable.ic_fuel_gauge_axio_16_8
+                    else -> R.drawable.ic_minus_dec
+                }
+            }
+            else -> {
+                when (level) {
+                    0 -> R.drawable.ic_fuel_gauge_0
+                    1 -> R.drawable.ic_fuel_gauge_1
+                    2 -> R.drawable.ic_fuel_gauge_2
+                    3 -> R.drawable.ic_fuel_gauge_3
+                    4 -> R.drawable.ic_fuel_gauge_4
+                    5 -> R.drawable.ic_fuel_gauge_5
+                    6 -> R.drawable.ic_fuel_gauge_6
+                    7 -> R.drawable.ic_fuel_gauge_7
+                    8 -> R.drawable.ic_fuel_gauge_8
+                    else -> R.drawable.ic_minus_dec
+                }
+            }
+        }
+    )
+}
