8359ec8e4e829d7d3f7f8fa61349f642d6265678
Kevin Beaton <kabton14@gmail.com>
2022-08-28 21:18:59 -0500

    Create database hilt module

---
+++ app/src/main/java/com/example/lateritia/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.example.lateritia.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.lateritia.database.VehicleDao
+import com.example.lateritia.database.VehicleRoomDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+    @Provides
+    fun provideVehicleDao(database: VehicleRoomDatabase): VehicleDao {
+        return database.vehicleDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): VehicleRoomDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            VehicleRoomDatabase::class.java,
+            "vehicle_database.db"
+        ).build()
+    }
+}
