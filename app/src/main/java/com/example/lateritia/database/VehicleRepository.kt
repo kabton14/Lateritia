d127fada3bd76ec105dc8a53ce9d800755b9743a
Kevin Beaton <kabton14@gmail.com>
2022-08-28 20:54:00 -0500

    Scope instance to the application container

--- app/src/main/java/com/example/lateritia/database/VehicleRepository.kt
+++ app/src/main/java/com/example/lateritia/database/VehicleRepository.kt
@@ -1,7 +1,9 @@
 package com.example.lateritia.database
 
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class VehicleRepository @Inject constructor(private val vehicleDao: VehicleDao) {
     suspend fun getVehicle(id: Long = 14): Vehicle? {
         return vehicleDao.loadVehicle(id)
